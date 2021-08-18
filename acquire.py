1950722468e5fbaaab5c253a06b84d5de4560813
Jeff Akins <jeffakins21@gmail.com>
2021-08-17 17:58:05 -0700

    Progress on Telco Project

--- acquire.py
+++ acquire.py
@@ -18,11 +18,16 @@ def get_connection(db, user=env.username, host=env.hostname, password=env.passwo
 #---------------------------Data Base Functions----------------------------------------
 
 # Function to retrieve the TELCO Data Set from CODEUP's mySQL Server 
-def get_titanic_data():
+def get_telco_data():
     if os.path.isfile('telco_churn.csv'):
         df = pd.read_csv('telco_churn.csv', index_col=0)  # If csv file exists read in data from csv file.
     else:
-        sql = 'SELECT * FROM passengers;'           # SQL query
+        sql = '''SELECT * 
+                FROM customers
+                JOIN contract_types USING(contract_type_id)
+                JOIN internet_service_types USING(internet_service_type_id)
+                JOIN payment_types USING(payment_type_id);''' 
+                                                    # SQL query
         db = 'telco_churn'                          # Database name
         df = pd.read_sql(sql, get_connection(db))   # Pandas DataFrame
         df.to_csv('telco_churn.csv')                # Cache Data
