a3c9ae55f0bab3e0aee991f4428e9efb3606c59c
Jeff Akins <jeffakins21@gmail.com>
2021-08-27 17:48:02 -0700

    Prepping telco data

--- prepare.py
+++ prepare.py
@@ -6,9 +6,11 @@ import acquire as aq
 
 #--------Functions-------
 # Function to clean the Iris Data
-def prep_iris():
-    iris_df = aq.get_iris_data()
-    iris_df = iris_df.drop_duplicates()
+def prep_telco():
+    df = aq.get_telco_data()                            # Using Acquire function to bring in telco data
+    df = df.drop_duplicates()                           # Dropping Duplicates
+    df.total_charges = df.total_charges.str.strip()     # Removing white space
+    df.total_charges = df.total_charges.replace('', 0)  # Replacing total_charges empty cells with 0 due to tenure = 0
     cols_to_drop = ['species_id', 'measurement_id']
     iris_df = iris_df.drop(columns=cols_to_drop)
     dummy_df = pd.get_dummies(iris_df[['species_name']], dummy_na=False)
